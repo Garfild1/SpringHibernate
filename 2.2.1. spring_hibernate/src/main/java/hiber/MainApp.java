1c7ad7e89d83c2ea5f5b9f062352686f22f1109e
Garfild1 <berngardtila@gmail.com>
2024-02-16 20:41:13 +0700

    first commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,56 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import hiber.service.UserServiceImp;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+
+      Car car1 = new Car("Model1", 1);
+      Car car2 = new Car("Model2", 2);
+      Car car3 = new Car("Model3", 3);
+      Car car4 = new Car("Model4", 4);
+
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar() );
+      }
+
+      System.out.println(userService.getUsersAndCars("Model2", 2));
+      System.out.println(userService.getUsersAndCars("Model4", 4));
+      System.out.println(userService.getUsersAndCars("Model1", 1));
+
+
+      try {
+         User notFound = userService.getUsersAndCars("Model",122);
+      } catch (NoResultException e) {
+         System.out.println("-----------");
+      }
+
+      context.close();
+   }
+}
